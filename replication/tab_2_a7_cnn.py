a03e73edbd8042b03ff5b72deb1aea465035eb17
Luka Biedebach <45386407+hiluka@users.noreply.github.com>
2023-07-25 10:50:09 +0200

    Add files via upload

--- replication/tab_2_a7_cnn.py
+++ replication/tab_2_a7_cnn.py
@@ -3,6 +3,19 @@
 # Christian Arnold, Luka Biedebach, Andreas Küpfer, and Marcel Neunhoeffer
 #--------------------------------------------------------------------------------
 
+#IMPORTS for Baselines
+from functions import load_data, preprocess_data, run_svc, run_dummy, run_randomforest, run_naivebayes
+import numpy as np
+np.set_printoptions(precision=15)
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem.snowball import EnglishStemmer, SpanishStemmer
+
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+
+# IMPORTS for CNN
 import os
 import tensorflow as tf
 import keras.backend as K
@@ -22,7 +35,7 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from gensim import models
 import gensim.downloader as api
 
-from utils.functions import *
+from functions import *
 
 import argparse
 
@@ -33,107 +46,125 @@ args = parser.parse_args()
 rerun = args.rerun
 
 if rerun:
-    print("Rerun flag true: tuning in progress...")
-if not rerun:
-    print("Rerun flag false: results loaded from files...")
-
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
-#load pre-trained model for correct language
-w2v_es = models.KeyedVectors.load_word2vec_format('sbw_vectors.bin', binary=True)
-w2v_en = api.load("word2vec-google-news-300")
-
-# initialize dictionary for countries/datasets
-countries = {"Ghana": "raw/gh-tweets.csv", "Philippines": "raw/ph-tweets.csv", "Venezuela": "raw/vz-tweets.csv"}
-
-#run several times with different param settings and seeds
-seeds = [20210101, 20210102, 20210103]
-
-#initialize result objects
-results_df = pd.DataFrame()
-results = []
-
-#loop over all countries
-for country, path in countries.items():
-
-    print("\nCurrent Country: " + country)
-
-    # initialize stopwords and stemmer in correct language
-    stops = set(stopwords.words("spanish")) if country == "Venezuela" else set(stopwords.words("english"))
-    stemmer = SpanishStemmer() if country == "Venezuela" else EnglishStemmer()
-    results_current = []
-
-    #load pre-trained model for correct language, to-do: aus der schleife holen
-    if country == "Venezuela":
-        w2v = w2v_es
-    else:
-        w2v = w2v_en
-
-    #preprocess the data
-    data = preprocess_data(path, stops, stemmer)
-
-    words=list(w2v.index_to_key)
-    vocab_len = len(w2v)
     
-    i=1
+    print("Rerun flag true: tuning in progress...")
+    
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-    for seed in seeds:
+    #load pre-trained model for correct language
+    # w2v_es = models.KeyedVectors.load_word2vec_format('sbw_vectors.bin', binary=True)
+    w2v_en = api.load("word2vec-google-news-300")
 
-        print("Run {i}/3".format(i=i))
 
-        #iterate through different random train test splits to capture model variation
-        X_train_vec, X_train_tfidf, \
-        X_test_vec, X_test_tfidf, \
-        y_train_vec, y_test = embedding_transform(data, w2v,words, seed)
-        
-        tuner = tune_model_cv(X_train_vec, y_train_vec, model= country+str(seed), runs=2, epochs=200)
-        
-        #build model with best params
-        best_hp = tuner.get_best_hyperparameters()[0]
-        model = tuner.hypermodel.build(best_hp)
-        
-        #set class weight
-        ratio_1 = 1.0 - len(y_train_vec[y_train_vec == 1]) / float(len(y_train_vec))  ## ratio of violence instances
-        ratio_0 = 1.0 - ratio_1
-        class_weight = {0: ratio_0, 1: ratio_1}
+    # initialize dictionary for countries/datasets
+    countries = {"Ghana": "raw/gh-tweets.csv", "Philippines": "raw/ph-tweets.csv"}
 
-        #fit model
-        model.fit([X_train_vec, X_train_vec, X_train_vec], y_train_vec, epochs=200, batch_size=64, class_weight=class_weight)
+    #run several times with different param settings and seeds
+    seeds = [20210101, 20210102, 20210103]
 
-        #classify sequences
-        y_pred = model.predict([X_test_vec, X_test_vec, X_test_vec])
-        y_pred =(y_pred>0.5)
+    #initialize result objects
+    results_df = pd.DataFrame()
+    results = []
 
-        results.append(best_hp+[print_stats(y_test, y_pred, model = "{c}_CNN_{p}".format(p="tuned params", c=country))])
-        pd.DataFrame(results, columns=["parameters", "model", "accuracy", "precision", "recall", "f1"]).to_csv("temp_results_{c}_run_{p}.csv".format(p=i, c=country))
-            
-        print(results)
-        
-        i+=1
-        
-#combine all results and calculate summary statistics
-results_df = pd.DataFrame(results, columns=["parameters", "model", "accuracy", "precision", "recall", "f1"])
-cnn_results.to_csv("results.csv")
-cnn_results = results_df.groupby(results_df["model"]).agg([np.mean, np.std])
-cnn_results.to_csv("final_results.csv")
+    #loop over all countries
+    for country, path in countries.items():
+
+        print("\nCurrent Country: " + country)
+
+        # initialize stopwords and stemmer in correct language
+        stops = set(stopwords.words("spanish")) if country == "Venezuela" else set(stopwords.words("english"))
+        stemmer = SpanishStemmer() if country == "Venezuela" else EnglishStemmer()
+        results_current = []
+
+        #load pre-trained model for correct language, to-do: aus der schleife holen
+        if country == "Venezuela":
+            w2v = w2v_es
+        else:
+            w2v = w2v_en
+
+        #preprocess the data
+        data = preprocess_data(path, stops, stemmer)
+
+        words=list(w2v.index_to_key)
+        vocab_len = len(w2v)
+
+        i=1
+
+        for seed in seeds:
+
+            print("Run {i}/3".format(i=i))
+
+            #iterate through different random train test splits to capture model variation
+            X_train_vec, X_train_tfidf, \
+            X_test_vec, X_test_tfidf, \
+            y_train_vec, y_test = embedding_transform(data, w2v,words, seed)
+
+            tuner = tune_model_cv(X_train_vec, y_train_vec, model= country+str(seed), runs=2, epochs=2)
+
+            #build model with best params
+            best_hp = tuner.get_best_hyperparameters()[0]
+            model = tuner.hypermodel.build(best_hp)
+
+            #set class weight
+            ratio_1 = 1.0 - len(y_train_vec[y_train_vec == 1]) / float(len(y_train_vec))  ## ratio of violence instances
+            ratio_0 = 1.0 - ratio_1
+            class_weight = {0: ratio_0, 1: ratio_1}
+
+            #fit model
+            model.fit([X_train_vec, X_train_vec, X_train_vec], y_train_vec, epochs=2, batch_size=64, class_weight=class_weight, verbose=0)
+
+            #classify sequences
+            y_pred = model.predict([X_test_vec, X_test_vec, X_test_vec])
+            y_pred =(y_pred>0.5)
+
+            #save tuned results
+            r = print_stats(y_test, y_pred, model = "CNN_tuned")
+            r.extend([country, seed, best_hp.values["filters"], [best_hp.values["kernel"],best_hp.values["kernel"]+1, best_hp.values["kernel"]+2] , best_hp.values["dropout"], best_hp.values["l2_reg_lambda"], best_hp.values["learning_rate"]])
+            results.append(r)
+
+            #default parameters
+            default_param = {"name": "default",
+                            "batch_size":64,
+                            "epochs":2,
+                            "filters":200,
+                            "kernel":[1,2,3],
+                            "dropout":0.5,
+                            "l2_reg_lambda": 0.01,
+                            "learning_rate": 0.001}
+
+            #train one CNN model on the "default" values
+            y_pred = run_CNN(X_train_vec, X_test_vec, y_train_vec, y_test, default_param, param_setting="default", country=country)
+
+            # save default result
+            d =  print_stats(y_test, y_pred, model = "CNN_default")
+            d.extend([country, seed, default_param["filters"], default_param["kernel"], default_param["dropout"], default_param["l2_reg_lambda"], default_param["learning_rate"]])
+            results.append(d)
+
+            #save temporary results
+            results_df_temp = pd.DataFrame(results, columns=['model', 'accuracy', 'precision', 'recall', 'f1', 'country', 'seed','filters', 'kernel', 'dropout', 'l2_reg_lambda', 'learning_rate']).round(3)
+            results_df_temp.to_csv("results/cnn_temp_{c}_{s}.csv".format(c=country, s=seed))
+
+            i+=1
+
+    #combine all results and calculate summary statistics
+    a7 = pd.DataFrame(results, columns=['model', 'accuracy', 'precision', 'recall', 'f1', 'country', 'seed','filters', 'kernel', 'dropout', 'l2_reg_lambda', 'learning_rate']).round(3)
+    a7.to_csv("results/cnn_results_a7.csv", index=False)
 
 if not rerun:
-    results_cnn = pd.read_csv("results/cnn_results.csv")
+    
+    print("Rerun flag false: results loaded from files...")
+    a7 = pd.read_csv("results/cnn_results_a7.csv")
+    
+
+a7["tuning"] = a7["model"].apply(lambda x: x.split("_")[1])
+tab2 = a7[["country", "tuning", "f1"]][a7["seed"]==20210101]
+tab2.to_csv("results/cnn_results_tab2.csv", index=False)
 
 ## Print and store detailed result scores
 print("--- Begin Table 2 ---")
-# Here CNN results
+print(tab2)
 print("--- End Table 2 ---")
-results_cnn.round(3).to_csv("results/cnn_results.csv", index=False)
-
-if rerun:
-    results_cnn_full.round(3).to_csv("results/cnn_results_full.csv", index=False)
-
-if not rerun:
-    results_tuning_svc = pd.read_csv("results/cnn_results_hyperparameter.csv")
 
 print("--- Begin Table A7 ---")
-print(results_tuning_cnn.round(3))
+print(a7)
 print("--- End Table A7 ---")
-
-results_tuning_cnn.round(3).to_csv("results/cnn_results_hyperparameter.csv", index=False)
